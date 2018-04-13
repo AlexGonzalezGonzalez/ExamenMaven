76141a71f9c1a87bffebdbb349e18c86d43c8bcb
estudios <estudios@MSI>
2018-04-13 09:40:39 +0200

    Obtenemos y mostramos el estado del cielo

--- src/main/java/com/mycompany/examenmaven/Examen.java
+++ src/main/java/com/mycompany/examenmaven/Examen.java
@@ -10,6 +10,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 /**
  *
@@ -24,19 +25,20 @@ public class Examen {
         try {
             //conectarse al documento de la pagina web
             Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").get();
-        
+
             //titulo del documento
             String titulo = doc.title();
             System.out.println(titulo);
             // Resultado: Vigo, España Pronóstico del tiempo y condiciones meteorológicas - The Weather Channel | Weather.com
-        
-        
-        
-        
-        
+
+            //Seleccionamos el elemento de la clase today_nowcard-phrase y cogemos su contenido
+            Elements estadoCielo = doc.select("div.today_nowcard-phrase");
+            System.out.println(estadoCielo.eachText());
+            //Muestra [Parcialmente nublado]
+
         } catch (IOException ex) {
             Logger.getLogger(Examen.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }
